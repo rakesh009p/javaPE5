79d3ded1244dbc27d1f6f30fac4ec529c01a1d7c
purimetla <rakeshpurimetla@gmail.com>
2019-07-26 08:19:03 +0530

    studentList added

--- src/main/java/com/stackroute/pe5/StudentList.java
+++ src/main/java/com/stackroute/pe5/StudentList.java
@@ -1,4 +1,71 @@
 package com.stackroute.pe5;
 
+import java.util.Comparator;
+
 public class StudentList {
+    private int id;
+    private String name;
+    private int age;
+
+    //constructor
+    public StudentList(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
+
+class StudentSorter implements Comparator<StudentList> {
+
+//    Sort the list based on their age in decreasing order, for student having
+//        same age, sort based on their name.
+//        f. For students having same name and age, sort them according to their ID.
+
+    @Override
+    public int compare(StudentList studentList, StudentList studentList1) {
+        if (studentList.getAge() == studentList1.getAge()) {
+            if (studentList.getName().equals(studentList1.getName()))
+                return studentList1.getId() - studentList.getId();
+            else
+                return studentList1.getName().compareTo(studentList1.getName());
+        } else {
+            return studentList1.getAge() - studentList.getAge();
+        }
+    }
+}
+
--- src/test/com/stackroute/pe5/StudentListTest.java
+++ src/test/com/stackroute/pe5/StudentListTest.java
@@ -2,18 +2,46 @@ package com.stackroute.pe5;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class StudentListTest {
     StudentList studentList;
+    StudentList studentList1;
 
     @Before
     public void setUp() throws Exception {
-        studentList=new StudentList();
+
     }
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void givenStudenListShouldCompareAndReturnSortedList() {
+
+        List<StudentList> students = new ArrayList<StudentList>();
+        StudentList obj1 = new StudentList(1, "Ajay", 27);
+        StudentList obj2 = new StudentList(2, "Sneha", 23);
+        StudentList obj3 = new StudentList(3, "Simran", 37);
+        StudentList obj4 = new StudentList(4, "Ajay", 22);
+        StudentList obj5 = new StudentList(5, "Ajay", 29);
+        StudentList obj6 = new StudentList(6, "Sneha", 22);
+        students.add(obj1);
+        students.add(obj2);
+        students.add(obj3);
+        students.add(obj4);
+        students.add(obj5);
+        students.add(obj6);
+        Collections.sort(students,new StudentSorter());
+        String expected="[Student{id=3, name='Simran', age=37}, Student{id=5, name='Ajay', age=29}, Student{id=1, name='Ajay', age=27}, Student{id=2, name='Sneha', age=23}, Student{id=4, name='Ajay', age=22}, Student{id=6, name='Sneha', age=22}]";
+        assertEquals(expected, students.toString());
+
+
     }
 }
